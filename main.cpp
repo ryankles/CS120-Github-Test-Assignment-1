6276398d7ddc133dba8ec5bdae7e43b98db2c74a
ryanreevess <ryanreevesyt@gmail.com>
2024-01-17 13:07:29 -0800

    Made first commit to test repository!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ryan Reeves on 1/17/24.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Ryan Reeves on 1/17/24.
+//
+
+#ifndef CS120_GITHUB_TEST_ASSIGNMENT_1_MAIN_H
+#define CS120_GITHUB_TEST_ASSIGNMENT_1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CS120_GITHUB_TEST_ASSIGNMENT_1_MAIN_H
